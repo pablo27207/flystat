e812d0a7334e7b7ee4df96a553f5bc8cd97ee08a
pablo27207@gmail.com <pablo27207@gmail.com@a8186771-bcca-7338-1b5a-87319350e61c>
2014-11-22 01:27:47 +0000

    slider y ejemplo de tabla y buscador

--- trunk/application/views/home.php
+++ trunk/application/views/home.php
@@ -2,8 +2,95 @@
 <aside class="right-side">
 
 <section class="content">
-             aca va el contenido de la pagina inicial
+              <!--=== Purchase Block ===-->
+    <div class="purchase">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-9 animated fadeInLeft">
+                    <span>Buscar vuelos</span>
+                    <p>Seleccione el criterio deseado para la busqueda de vuelos.</p>
+                </div>            
+                <div class="col-md-3 btn-buy animated fadeInRight">
+                    <a href="#" class="btn-u btn-u-lg"><i class="fa fa-cloud-download"></i> Alexis!</a>
+                </div>
+            </div>
+        </div>
+    </div><!--/row-->
+    <!-- End Purchase Block -->
+      <!--=== Search Block Version 2 ===-->
+    <div class="search-block-v2">
+        <div class="container">
+            <div class="col-md-6 col-md-offset-3">
+                <h2>Buscar</h2>
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Ingrese el valor a buscar segun el criterio seleccionado...">
+                    <span class="input-group-btn">
+                     <button type="button" class="btn-u dropdown-toggle" data-toggle="dropdown">
+                            Buscar por... 
+                            <i class="fa fa-angle-down"></i>
+                        </button>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="#"><i class="fa fa-plane"></i> N&uacute;mero vuelo</a></li>
+                            <li><a href="#"><i class="fa fa-globe"></i> Origen</a></li>
+                            <li><a href="#"><i class="fa fa-globe"></i> Destino</a></li>
+                              <li><a href="#"><i class="fa fa-calendar"></i> fecha</a></li>
+                           
+                        </ul>
+                 <!--  <button class="btn-u" type="button"><i class="fa fa-search"></i></button> --> 
+                    </span>
+                </div>
+            </div>
+        </div>    
+    <!--Basic Table-->
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>First Name</th>
+                            <th class="hidden-sm">Last Name</th>
+                            <th>Username</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td class="hidden-sm">Otto</td>
+                            <td>@mdo</td>
+                            <td><span class="label label-warning">Expiring</span></td>                          
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Jacob</td>
+                            <td class="hidden-sm">Thornton</td>
+                            <td>@fat</td>
+                            <td><span class="label label-success">Success</span></td>                           
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Larry</td>
+                            <td class="hidden-sm">the Bird</td>
+                            <td>@twitter</td>
+                            <td><span class="label label-danger">Error!</span></td>                         
+                        </tr>
+                        <tr>
+                            <td>4</td>
+                            <td>htmlstream</td>
+                            <td class="hidden-sm">Web Design</td>
+                            <td>@htmlstream</td>
+                            <td><span class="label label-info">Pending..</span></td>                            
+                        </tr>
+                    </tbody>
+                </table>
+                <!--End Basic Table-->
+    
+    </div><!--/container--> 
+     
+     
+
+     
      
 </section>            
 </aside>
-                
+
--- trunk/application/views/includes/footer_view.php
+++ trunk/application/views/includes/footer_view.php
@@ -108,15 +108,23 @@
 <script type="text/javascript" src="assets/js/plugins/flexslider/jquery.flexslider-min.js"></script>
 <script type="text/javascript" src="assets/js/plugins/parallax-slider/modernizr.js"></script>
 <script type="text/javascript" src="assets/js/plugins/parallax-slider/jquery.cslider.js"></script>
+
+
+<!-- SLIDER REVOLUTION 4.x SCRIPTS  -->
+<script type="text/javascript" src="assets/js/plugins/revolution-slider/jquery.themepunch.plugins.min.js"></script>
+<script type="text/javascript" src="assets/js/plugins/revolution-slider/jquery.themepunch.revolution.min.js"></script>
 <!-- JS Page Level -->           
 <script type="text/javascript" src="assets/js/app.js"></script>
 <script type="text/javascript" src="assets/js/pages/index.js"></script>
+
+
+
 <script type="text/javascript">
-    jQuery(document).ready(function() {
-      	App.init();
-        App.initSliders();
-        Index.initParallaxSlider();        
-    });
+jQuery(document).ready(function() {
+    App.init();
+    Index.initRevolutionSlider();
+ 
+});
 </script>
 <!--[if lt IE 9]>
     <script src="assets/plugins/respond.js"></script>
